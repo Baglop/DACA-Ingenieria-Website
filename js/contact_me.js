1ffe695e7d9a86b60e54a2b2306e5932849fdcbe
Bacilio Gonzalez Lopez <archi6661@hotmail.com>
2019-03-29 02:38:03 -0600

    Final_Version

--- js/contact_me.js
+++ js/contact_me.js
@@ -10,7 +10,6 @@ $(function() {
       // get values from FORM
       var name = $("input#name").val();
       var email = $("input#email").val();
-      var phone = $("input#phone").val();
       var message = $("textarea#message").val();
       var firstName = name; // For Success/Failure Message
       // Check for white space in name for Success/Fail message
@@ -24,7 +23,6 @@ $(function() {
         type: "POST",
         data: {
           name: name,
-          phone: phone,
           email: email,
           message: message
         },
@@ -46,7 +44,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text( firstName + ", tenemos probelmas para enviar el correo, intente de nuevo"));
+          $('#success > .alert-danger').append($("<strong>").text( firstName + ", tenemos problemas para enviar el correo, intente de nuevo"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -2,25 +2,34 @@
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
    }
-   
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
+$utf8_message = utf8_decode($message);
+$utf8_name = utf8_decode($name);
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+$to = 'contacto@dacaingenieria.com.mx'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Email enviado desde pagina web por:  $utf8_name";
+$email_body = "<html><body>";
+$email_body .= "<h2>Correo recibido desde el formulario de contacto</h2>";
+$email_body .= "<p><strong>Nombre:</strong> $utf8_name </p>";
+$email_body .= "<p><strong>Email:</strong> $email_address </p>";
+$email_body .= "<hr>";
+$email_body .= "<strong>Mensaje:</strong><br>";
+$email_body .= "<pre style=\"font-family: Arial\">$utf8_message</pre>";
+$email_body .= "</body></html>";
+
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+$headers .= "From: no-reply@dacaingenieria.com.mx" . "\r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+
+
+if(mail($to,$email_subject,$email_body,$headers))return true; else return false;
 ?>
